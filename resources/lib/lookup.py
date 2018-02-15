b6991bf8e1dd5edae2cc27da2b03adcd41655a0b
kodiful <uchiyama@mac.com>
2018-02-15 18:18:01 +0900

    fix lookup.py

--- resources/lib/lookup.py
+++ resources/lib/lookup.py
@@ -37,9 +37,9 @@ class Lookup:
         self.cache = PhoneBook('cache.json')
         name = None
         key = None
-        m = re.compile('"(.*)"\s*<(.*)>').search(uri)
-        if m and m.group(1):
-            key = m.group(1)
+        m = re.compile('(?:"(.*)"|(.*))\s*<(sip:(.*)@.*|.*)>').search(uri)
+        if m:
+            key = m.group(1) or m.group(2) or m.group(4) or m.group(3)
             # 0で始まって9桁以上ある場合は電話番号として先ず電話帳、次にキャッシュ、最後にウェブを検索
             if re.compile('^0[0-9]{8,}').search(key):
                 # 電話帳を検索
@@ -54,16 +54,27 @@ class Lookup:
                             pass
                         else:
                             if isinstance(name, unicode): name = name.encode('utf-8')
-                            # キャッシュに追加
-                            if cache: self.cache.update(key, name)
+                        # キャッシュに追加
+                        if cache: self.cache.update(key, name or 'n/a')
+                    elif name == 'n/a':
+                        name = None
+            # 以下は
+            # http://www.ttc.or.jp/jp/document_list/pdf/j/STD/JJ-90.22v1.1.pdf
+            # による
             elif key == 'Anonymous':
-                #name = '非通知'
+                # ユーザ拒否のため通知不可
                 name = addon.getLocalizedString(32908).encode('utf-8')
             elif key == 'Coin line/payphone':
-                #name = '公衆電話'
+                # 公衆電話発信のため通知不可
                 name = addon.getLocalizedString(32909).encode('utf-8')
+            elif key == 'Unavailable':
+                # 通知可能な情報が存在しない
+                name = addon.getLocalizedString(32910).encode('utf-8')
+            elif key == 'Interaction with other service':
+                # サービス競合のため通知不可
+                name = addon.getLocalizedString(32910).encode('utf-8')
         # 検索結果
         name = name or key or uri
         # 履歴に追加
-        if history: self.history.append(uri, key, name)
+        if history: self.history.append(uri, key or 'n/a', name or 'n/a')
         return name
