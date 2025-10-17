fb291b28a1d6fc6f99543a1e990787b3e59f8917
kodiful <uchiyama@mac.com>
2025-10-17 10:19:02 +0900

    search phone number by selenium

--- resources/lib/search.py
+++ resources/lib/search.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import re
 
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
 from resources.lib.common import Common
+
+sys.path.append(Common.RESOURCES_PATH)
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 
 def search(key):
@@ -13,23 +15,20 @@ def search(key):
         # 番号案内ページのURL
         url = 'https://www.telnavi.jp/phone/%s' % key
         # ページ読み込み
-        res = urlopen(url)
-        status = res.getcode()
-        if status == 200:
-            # タグ抽出
-            html = res.read()
-            soup = BeautifulSoup(html, 'html.parser')
-            title = soup.find('title').get_text()
-            # 電話番号08012000021の情報は？｜電話番号検索の電話帳ナビ
-            # 電話番号0120102030は司法書士法人新宿事務所
-            if re.match(r'電話番号[0-9]+は', title):
-                name = re.sub(r'電話番号[0-9]+は', '', title)
-            else:
-                name = None
+        opts = Options()
+        opts.add_argument('-headless')
+        driver = webdriver.Firefox(options=opts)
+        driver.get(url)
+        title = driver.title
+        driver.quit()
+        # スクレイピング
+        # 電話番号08012000021の情報は？｜電話番号検索の電話帳ナビ
+        # 電話番号0120102030は司法書士法人新宿事務所
+        if re.match(r'電話番号[0-9]+は', title):
+            name = re.sub(r'電話番号[0-9]+は', '', title)
         else:
             name = None
-        res.close()
-        Common.log('key=%s name=%s status=%s url=%s' % (key, name, status, url))
+        Common.log('key=%s name=%s url=%s' % (key, name, url))
     except Exception as e:
         name = None
         Common.log(e)
